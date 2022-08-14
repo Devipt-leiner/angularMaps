163746b29fcc1bac61db701af18a07ae16569373
Leiner Barrios Medina <leiner.barrios@inglesparatodos.edu.co>
2022-08-14 16:15:36 -0500

    Full screen map, control zoom map by buttons and input range

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoibGVpbmVyZGV2IiwiYSI6ImNsNnRwcXN0dzFxdzYzbHIzOW9ocnJ0Y3EifQ.DfPl1BAo5E3OSt9DZQ1fMA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoibGVpbmVyZGV2IiwiYSI6ImNsNnRwcXN0dzFxdzYzbHIzOW9ocnJ0Y3EifQ.DfPl1BAo5E3OSt9DZQ1fMA'
 };
 
 /*
